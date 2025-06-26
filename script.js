62872e4ea9bac9eeac142ed8659eeec0b9ddca03
Raj Yadav <rahulyaxav72@gmail.com>
2025-06-26 20:32:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const track = document.getElementById('carouselTrack');
+    const slides = document.querySelectorAll('.carousel-slide');
+    let currentSlide = 0;
+
+    function moveSlide(direction) {
+      currentSlide = (currentSlide + direction + slides.length) % slides.length;
+      track.style.transform = `translateX(-${currentSlide * 100}%)`;
+      
+    }
+
+    setInterval(() => moveSlide(1), 5000);
+
+
+      const nav = document.querySelector('.mobile-nav');
+      const ham = document.getElementById('hamburger');
+      const cross = document.getElementById("cross");
+    ham.addEventListener("click" , () =>{
+        nav.classList.remove("close");
+        nav.classList.add("open");
+    })
+    cross.addEventListener("click" , () => {
+        nav.classList.remove("open");
+        nav.classList.add("close");
+    })
+$(document).ready(function(){
+  $('.job-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    responsive:{
+      0:{ items:1 },
+      768:{ items:2 },
+      1024:{ items:3 }
+    }
+  });
+});
+
+ const hiddenSections = document.querySelectorAll('.hidden-section');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+      }
+    });
+  }, {
+    threshold: 0.3
+  });
+
+  hiddenSections.forEach(section => {
+    observer.observe(section);
+  });
